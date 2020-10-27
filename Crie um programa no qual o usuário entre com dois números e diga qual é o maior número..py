b5df0bb2e6b156885cf25c4154a7f3c4c44b6215
Bloodhard <luanguimaraes031101@gmail.com>
2020-10-27 19:41:02 +0000

    Godhard01

--- Crie um programa no qual o usuário entre com dois números e diga qual é o maior número..py
+++ Crie um programa no qual o usuário entre com dois números e diga qual é o maior número..py
@@ -1,1 +1,18 @@
+#Crie um programa no qual o usuário entre com dois números e diga qual é o maior número
+#Aluno: Luan Fernando Duarte Guimarães
 
+valid = True
+while valid == True:
+    x = int(input('n1: '))
+    y = int(input('n2: '))
+    if x > y:
+        print(x,'é maior do que ', y)
+    elif x < y:
+        print(y,'é maior do que ', x)
+    else:
+        print(x,'e', y, 'são iguais')
+    valid = input('deseja continuar?s/n: ')
+    if valid == 's':
+        valid = True
+    else:
+        valid = False
