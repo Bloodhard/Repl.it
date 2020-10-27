b530ed96341b52a3d6361ba9c8731cbecff039e9
Bloodhard <luanguimaraes031101@gmail.com>
2020-10-27 19:30:50 +0000

    Exercices.py

--- Exercicio1.py
+++ Exercicio1.py
@@ -1,1 +1,17 @@
-
+numero = int(input('Digite um numero:'))
+divisores = 0
+cont = int(input('Até: '))
+while cont >= numero:
+  for divisor in range(1, numero):
+      if numero % divisor == 0:
+          divisores = divisores + 1
+          if divisores > 1:
+            break
+  if divisores > 1:
+    print(numero,'não é primo')
+  elif numero == 1:
+    print(numero,'não é primo')
+  else:
+    print(numero,'é primo')
+  divisores = 0
+  numero = numero + 1  
