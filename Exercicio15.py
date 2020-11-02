e1d77bc558ec867d021e122ea6df0f5fddab946b
Godhard <51868742+Bloodhard@users.noreply.github.com>
2020-11-02 12:37:45 -0300

    Update Exercicio15.py

--- Exercicio15.py
+++ Exercicio15.py
@@ -1,5 +1,5 @@
-r= 'n'
-while r != 'S' and r != 's':
+r= 'N'
+while r != 'S' or r != 's':
   n1=float(input('Digite o Primeiro número: '))
   n2=float(input('Digite o Segundo número: '))
 
@@ -12,12 +12,11 @@ while r != 'S' and r != 's':
     print(n1*n2)
   elif op == '4':
     print(n1/n2)
-  if op == '5':
+  elif op == '5':
     n1 < n2
     print(n2)
-  if op == '6':
+  elif op == '6':
     n1 > n2
     print(n1)
-  r=input('Se desejar sair do código digite "S": ')
-[18:59]
-exercicio 15 ai
+  r=input('Se desejar sair do código digite "S":')
+
