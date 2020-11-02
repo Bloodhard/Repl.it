7ee87c56f080cf40e80f3201db473706bc2b07f1
Godhard <51868742+Bloodhard@users.noreply.github.com>
2020-11-02 12:21:56 -0300

    Update Exercicio17.py

--- Exercicio17.py
+++ Exercicio17.py
@@ -1,5 +1,5 @@
 op='n'
-while op !='S' and op!='s':
+while op !='S' or op!='s':
   notas=input('Deseja utilizar quantas notas para a média?\n 1 - Para 2 notas;\n 2- Para 3 notas;\n 3- Para 4 Notas \n: ')
 
   if notas == '1':
@@ -23,3 +23,11 @@ while op !='S' and op!='s':
     media= n1+n2+n3+n4
     resultado= media/4
     print('Sua média é: ',resultado)
+   
+  if resultado >=7:
+    print('\nVocê foi aprovado com média: ',resultado)
+  elif resultado < 4:
+    print('\nVocê foi reprovado, sua média foi de: ',resultado)
+  else:
+    print('\nVocê está de exame, sua nota é de: ',resultado)
+  op=input('\nSe desejar sair do programa, aperte "S", aperte qualquer outra tecla para continuar:\n ')
