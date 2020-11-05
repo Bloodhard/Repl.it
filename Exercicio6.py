e7c96462214d6f32f93ea2b934e577d98f5e536b
Godhard <51868742+Bloodhard@users.noreply.github.com>
2020-11-04 21:18:28 -0300

    Create Exercicio6.py

---
+++ Exercicio6.py
@@ -0,0 +1,13 @@
+dividendo= 1000
+divisor= 1
+resultado= 0
+
+while divisor <= 50:
+  resultado+= dividendo/divisor
+  print(dividendo, '/', divisor,'=', resultado )
+  dividendo-=3
+  divisor+= 1
+print('O resultado das expressões foram:', resultado)
+  
+
+
